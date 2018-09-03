d8fb8e1e0fd25fc2b8759468d63c047245105174
mraarm <arnmnra@gmail.com>
2018-09-03 20:40:09 +0200

    Bcrypt, Retoques finales Singup y Login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,24 @@
   /* ----------------- REQUIRES ----------------- */
-
-// EXPRESS: Framework para simplifica la creacion del servidor
-const express = require('express'); 
-
-// MOONGOOSE: Sirve para establecer la connexion a la base de datos
-const mongoose = require('mongoose');
-
-// MONGO MODEL: Cargamos los modelos de moongoose
-const User = require('./../models/user'); 
-
-// BCRYPT: Srive para crear un hash y asi codificar la contraseña
-const bcrypt =  require('bcrypt');
-
-/* -----------------    FIN   -----------------*/
-
-/* -----------------    CONNECTION MONGOOSE   -----------------*/
-
-// Creamos la connexion con la base de datos MongoDB, en el localhost
-mongoose.connect('mongodb://localhost/nodejs', { useNewUrlParser: true })
-.then(() => {
+  
+  // EXPRESS: Framework para simplifica la creacion del servidor
+  const express = require('express'); 
+  
+  // MOONGOOSE: Sirve para establecer la connexion a la base de datos
+  const mongoose = require('mongoose');
+  
+  // MONGO MODEL: Cargamos los modelos de moongoose
+  const User = require('./../models/user'); 
+  
+  // BCRYPT: Srive para crear un hash y asi codificar la contraseña
+  const bcrypt =  require('bcrypt');
+  
+  /* -----------------    FIN   -----------------*/
+  
+  /* -----------------    CONNECTION MONGOOSE   -----------------*/
+  
+  // Creamos la connexion con la base de datos MongoDB, en el localhost
+  mongoose.connect('mongodb://localhost/nodejs', { useNewUrlParser: true })
+  .then(() => {
     console.log("Conexion con MongoDB correcta");
 })
 .catch((err) => {
@@ -49,76 +49,69 @@ router.get('/login',(req, res) => {
 
 // Ruta para el Login, valida el usuario i la contraseña
 router.post('/login', (req, res) => {
-
-    console.log("Comprovando datos para hacer el LOGIN\n");
+    
+    console.log("\nComprovando datos para hacer el LOGIN\n");
     // Creamos una variable donde se guardara los parametros
     let body = ''; 
-
+    
     // Cada vez que recibimos un chunk (stream que contiene los datos)
     req.on('data', (data) => {
+        
         console.log("Recibiendo datos", data.toString());
+        
         body += data.toString();
     }); 
-
+    
     // Cuando acabe de enviarnos los datos, enviara el evento end
     req.on('end', () => {
-
+        
         console.log("Recepcion de datos finalizada");
-
+        
         // Convertimos la informacion a JSON 
         body = toJSON(body);
-
+        
         // Variable donde guardamos el usuario o el username
         let find = {}; 
         
-        // Por defecto buscaremos por el usuario
-        find['username'] = body.username; 
-
         // En caso de que nos den el mail
-        if ( (body.username).indexOf('@') === -1 ) {
- 
-            console.log("Han usado el mail @");
+        if ( (body.username).indexOf('%40') !== -1 ) {
+            
+            console.log("\nHan usado el mail @\n");
             
             // Buscaremos por el mail
             find["email"] = body.username;
-
+            
+        } else {
+            // Por defecto buscaremos por el usuario
+            find['username'] = body.username; 
         }
-
+        
         // Comprovamos los valores 
-        User.findOne( body, (err, doc) => {          
-            
-            // Si hay un error lo buscamos
-            if (err) console.log(err); 
-            
-            // Si el documento no existe, no existe el usuario
-            if (doc === []) res.render('login', { message: " No existe el usuario " }); 
-            else { 
-                bcrypt.compare(body.username, doc.password, (err, same) => {
-
-                    // Creamos un string para enviar los datos del usuario que hace el login
-                    let user = ""; 
-                    
-                    if (same) {
-                        
-                        for (key in doc) {
-                            user += key + "=" + doc.key + "&"; 
-                        }
-
-                        // Redirecionamos a la pagina de el usuario
-                        res.redirect('/profile?' + user); 
-
-                    } else {
-
-                        // La contraseña no coincide 
-                        res.render('login', { message: " Contraseña equivocada " });
-
-                    }
-                }); 
-            }
-        });
+        let user = null; 
         
         // Comprobamos que la informacion es correcta antes de hacer el login
-        
+        User.find(find, 'password').cursor()
+        .on('data', (data) => {
+            user = data; 
+            console.log( "JSON", user); 
+        })
+        .on('end', () => { 
+            // Si no hay documento
+            if (user) {
+                
+                let hash = user.password;
+                let password = body.password; 
+                
+                // console.log(hash, password);
+                
+                
+                if (bcrypt.compareSync(body.password, user["password"])) {
+                    res.render('profile'); 
+                } else {
+                    res.render('login', { message: "Contraseña incorrecta" });
+                }
+            } else res.render('login', { message: "El usuario no existe" }); 
+        }); 
     }); 
 }); 
 
@@ -130,48 +123,48 @@ router.get('/singup',(req, res) => {
 
 // Ruta para el Singup, guarda los datos en la base de datos
 router.post('/singup', (req, res) => {
-
+    
     console.log("Comprovando datos para hacer el SINGUP\n");
-
+    
     // Creamos una variable donde se guardara los parametros
     let body = ''; 
-
+    
     // Cada vez que recibimos un chunk (stream que contiene los datos)
     req.on('data', (data) => {
-
+        
         console.log("Recibiendo datos");
-
+        
         body += data.toString();
-
+        
     }); 
-
+    
     // Cuando acabe de enviarnos los datos, enviara el evento end
     req.on('end', () => {
-
+        
         console.log("Comprovando datos para hacer el SINGUP\n");
         
         // Creamos el objeto que guardaremos
         body = toJSON(body); 
-
+        
         // Creamos el usuario
         let newUser = new User(body); 
-
+        
         // Guardamos el usuario
         newUser.save()
-            .then( () => { 
-                res.render('profile');
-            })
-            .catch( (err) => {
-                if ( (err.errmsg).indexOf('username') !== -1 ) res.render('singup', { email: "", username: " Username usado" } );
-                else if ( (err.errmsg).indexOf('email') !== -1 ) res.render('singup', { email: "Email usado", username: "" } );
-                else console.log(err); 
-            }); 
+        .then( () => { 
+            res.render('profile');
+        })
+        .catch( (err) => {
+            if ( (err.errmsg).indexOf('username') !== -1 ) res.render('singup', { email: "", username: " Username usado" } );
+            else if ( (err.errmsg).indexOf('email') !== -1 ) res.render('singup', { email: "Email usado", username: "" } );
+            else console.log(err); 
+        }); 
     });
 }); 
 
 // Ruta para el perfil
 router.get('/perfil',(req, res) => {
-
+    
     console.log("GET - To file chat.ejs");
     conosle.log(req.body); 
     
@@ -189,7 +182,7 @@ router.get('/perfil',(req, res) => {
 * @param {String} data 
 */
 function toJSON(data) {
-
+    
     // Convertimos los datos passados a JSON
     let info = {}; 
     
@@ -197,12 +190,12 @@ function toJSON(data) {
     // creamos un array con cada pareja de valores y entonces 
     // montamos el objeto json
     data.split('&').forEach(element => {
-    
+        
         let partial = element.split('=');
-    
+        
         // Añadimos cada clave, valor
         info[partial[0]] = partial[1]; 
-    
+        
     }); 
     
     // Devolvemos el obejto
